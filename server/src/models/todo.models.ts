a728cdd9d135a59d414311bd278fab37c4eca7ab
Rajan Kumar <raorajan9576@gmail.com>
2026-03-15 12:12:11 +0530

    models created fro the todos

---
+++ server/src/models/todo.models.ts
@@ -0,0 +1,54 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export enum TaskStatus {
+  TODO = 'todo',
+  IN_PROGRESS = 'in-progress',
+  DONE = 'done'
+}
+
+export enum TaskPriority {
+  LOW = 'low',
+  MEDIUM = 'medium',
+  HIGH = 'high'
+}
+
+export interface ITask extends Document {
+  title: string;
+  description?: string;
+  status: TaskStatus;
+  priority: TaskPriority;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const TaskSchema: Schema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: ''
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(TaskStatus),
+      default: TaskStatus.TODO
+    },
+    priority: {
+      type: String,
+      required: true,
+      enum: Object.values(TaskPriority),
+      default: TaskPriority.MEDIUM
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<ITask>('Task', TaskSchema);
